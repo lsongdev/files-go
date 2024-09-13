a61eb3f71279e5908f5fcf10faa3149c9920bcce
Lsong <song940@gmail.com>
2024-09-14 01:59:31 +0800

    update

--- go.mod
+++ go.mod
@@ -1,9 +1,10 @@
 module github.com/song940/files-go
 
-go 1.22.0
+go 1.22.4
 
 require (
 	github.com/karrick/godirwalk v1.17.0 // indirect
+	github.com/lsongdev/apk-go v0.0.0-20240913172039-51829f301658 // indirect
 	github.com/mattn/go-sqlite3 v1.14.23 // indirect
 	github.com/song940/fileinfo-go v0.0.0-20231225143900-45d5120787ba // indirect
 	github.com/song940/tmdb-go v0.0.0-20240306070408-2b1e84e68019 // indirect
--- main.go
+++ main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"crypto/md5"
 	"database/sql"
 	"encoding/json"
+	"fmt"
+	"html/template"
+	"image/png"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 
+	"github.com/lsongdev/apk-go/apk"
 	_ "github.com/mattn/go-sqlite3"
 )
+
+type H = map[string]interface{}
 
 type FileInfo struct {
 	Name  string `json:"name"`
@@ -20,15 +29,16 @@ type FileInfo struct {
 }
 
 type FileServer struct {
-	db *sql.DB
+	db   *sql.DB
+	root string
 }
 
-func NewFileServer() (server *FileServer, err error) {
+func NewFileServer(root string) (server *FileServer, err error) {
 	db, err := sql.Open("sqlite3", ":memory:")
 	if err != nil {
 		return nil, err
 	}
-	server = &FileServer{db: db}
+	server = &FileServer{db, root}
 	server.initDB()
 	return
 }
@@ -46,20 +56,31 @@ func (server *FileServer) initDB() error {
 	return err
 }
 
-func (fs *FileServer) ScanDirectory(root string) error {
-	return filepath.Walk(root, func(filename string, info os.FileInfo, err error) error {
+func (server *FileServer) ScanDirectory() error {
+	return filepath.Walk(server.root, func(filename string, info os.FileInfo, err error) error {
 		if err != nil {
+			log.Println(err)
 			return err
 		}
+		// TODO: when is complete?
 		dir, name := filepath.Split(filename)
-		dir, _ = filepath.Rel(root, dir)
-		log.Println(dir, name)
-		_, err = fs.db.Exec("INSERT INTO files (name, path, is_dir, size) VALUES (?, ?, ?, ?)", name, dir, info.IsDir(), info.Size())
+		dir, _ = filepath.Rel(server.root, dir)
+		if dir == ".." {
+			// Ignore parent directory
+			return nil
+		}
+		if dir == "." {
+			dir = ""
+		}
+		dir = "/" + dir
+		log.Println("Scanning", dir, name)
+		_, err = server.db.Exec("INSERT INTO files (name, path, is_dir, size) VALUES (?, ?, ?, ?)", name, dir, info.IsDir(), info.Size())
 		return err
 	})
 }
 
 func (fs *FileServer) ListFiles(path string, offset, size int) (files []FileInfo, err error) {
+	// log.Println(path, offset, size)
 	rows, err := fs.db.Query("SELECT name, path, is_dir, size FROM files WHERE path = ? LIMIT ? OFFSET ?", path, size, offset)
 	if err != nil {
 		return
@@ -75,7 +96,31 @@ func (fs *FileServer) ListFiles(path string, offset, size int) (files []FileInfo
 	return
 }
 
-func (fs *FileServer) IndexHandler(w http.ResponseWriter, r *http.Request) {
+// Render renders an HTML template with the provided data.
+func (s *FileServer) Render(w http.ResponseWriter, name string, data H) {
+	if data == nil {
+		data = H{}
+	}
+	tmpl, err := template.ParseFiles("templates/layout.html", "templates/"+name+".html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = tmpl.ExecuteTemplate(w, "layout", data)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (server *FileServer) ApiHandler(w http.ResponseWriter, r *http.Request) {
+	files := server.ListFilesHandler(w, r)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(files)
+}
+
+func (server *FileServer) ListFilesHandler(w http.ResponseWriter, r *http.Request) (files []FileInfo) {
 	page, _ := strconv.Atoi(r.URL.Query().Get("page"))
 	if page < 1 {
 		page = 1
@@ -86,28 +131,76 @@ func (fs *FileServer) IndexHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	path := r.URL.Query().Get("path")
 	if path == "" {
-		path = "."
+		path = "/"
 	}
 	offset := (page - 1) * pageSize
-	files, err := fs.ListFiles(path, offset, pageSize)
+	files, err := server.ListFiles(path, offset, pageSize)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(files)
+	return
+}
+
+func (server *FileServer) IndexView(w http.ResponseWriter, r *http.Request) {
+	files := server.ListFilesHandler(w, r)
+	server.Render(w, "index", H{
+		"files": files,
+	})
+}
+
+func (server *FileServer) IconHandler(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Query().Get("path")
+	filename := filepath.Join(server.root, path)
+	stat, err := os.Stat(filename)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if stat.IsDir() {
+		http.Redirect(w, r, "https://cdn-icons-png.freepik.com/256/12532/12532956.png", http.StatusSeeOther)
+		return
+	}
+
+	ext := strings.ToLower(filepath.Ext(filename))
+	switch ext {
+	case ".jpg", ".jpeg", ".png":
+		http.ServeFile(w, r, filename)
+	case ".apk":
+		hasher := md5.New()
+		io.WriteString(hasher, filename)
+		cachePath := fmt.Sprintf("/tmp/%x.png", hasher.Sum(nil))
+		pkg, _ := apk.Open(filename)
+		defer pkg.Close()
+		icon, _ := pkg.Icon(nil)
+		f, _ := os.Create(cachePath)
+		png.Encode(f, icon)
+		http.ServeFile(w, r, cachePath)
+	default:
+		http.Redirect(w, r, "https://cdn-icons-png.flaticon.com/256/607/607674.png", http.StatusSeeOther)
+	}
+}
+
+func (server *FileServer) FileView(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Query().Get("path")
+	filename := filepath.Join(server.root, path)
+	http.ServeFile(w, r, filename)
 }
 
 func main() {
-	server, err := NewFileServer()
+	root := "/Volumes/software/Mobile/APKs"
+	server, err := NewFileServer(root)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	root := "/Volumes/nsfw"
-	go server.ScanDirectory(root)
+	go server.ScanDirectory()
 
-	http.HandleFunc("/", server.IndexHandler)
+	http.HandleFunc("/", server.IndexView)
+	http.HandleFunc("/view", server.FileView)
+	http.HandleFunc("/icon", server.IconHandler)
+	http.HandleFunc("/api", server.ApiHandler)
 	log.Println("Server is running on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
