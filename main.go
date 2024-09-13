637f2f3b12c3fea0a13563adfcc7f02cc826cfec
Lsong <song940@gmail.com>
2024-09-13 16:31:40 +0800

    update

--- main.go
+++ main.go
@@ -19,16 +19,22 @@ type FileInfo struct {
 	Size  int64  `json:"size"`
 }
 
-var db *sql.DB
+type FileServer struct {
+	db *sql.DB
+}
 
-func initDB() {
-	var err error
-	db, err = sql.Open("sqlite3", ":memory:")
+func NewFileServer() (server *FileServer, err error) {
+	db, err := sql.Open("sqlite3", ":memory:")
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
+	server = &FileServer{db: db}
+	server.initDB()
+	return
+}
 
-	_, err = db.Exec(`
+func (server *FileServer) initDB() error {
+	_, err := server.db.Exec(`
 		CREATE TABLE files (
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
 			name TEXT,
@@ -37,12 +43,10 @@ func initDB() {
 			size INTEGER
 		)
 	`)
-	if err != nil {
-		log.Fatal(err)
-	}
+	return err
 }
 
-func scanDirectory(root string) error {
+func (fs *FileServer) ScanDirectory(root string) error {
 	return filepath.Walk(root, func(filename string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
@@ -50,46 +54,44 @@ func scanDirectory(root string) error {
 		dir, name := filepath.Split(filename)
 		dir, _ = filepath.Rel(root, dir)
 		log.Println(dir, name)
-		_, err = db.Exec("INSERT INTO files (name, path, is_dir, size) VALUES (?, ?, ?, ?)", name, dir, info.IsDir(), info.Size())
-		if err != nil {
-			return err
-		}
-		return nil
+		_, err = fs.db.Exec("INSERT INTO files (name, path, is_dir, size) VALUES (?, ?, ?, ?)", name, dir, info.IsDir(), info.Size())
+		return err
 	})
 }
 
-func ListFiles(path string, offset int, size int) (files []FileInfo, err error) {
-	rows, err := db.Query("SELECT name, path, is_dir, size FROM files WHERE path = ? LIMIT ? OFFSET ?", path, size, offset)
+func (fs *FileServer) ListFiles(path string, offset, size int) ([]FileInfo, error) {
+	rows, err := fs.db.Query("SELECT name, path, is_dir, size FROM files WHERE path = ? LIMIT ? OFFSET ?", path, size, offset)
 	if err != nil {
-		return
+		return nil, err
 	}
 	defer rows.Close()
+	var files []FileInfo
 	for rows.Next() {
 		var file FileInfo
-		err = rows.Scan(&file.Name, &file.Path, &file.IsDir, &file.Size)
-		if err != nil {
-			return
+		if err := rows.Scan(&file.Name, &file.Path, &file.IsDir, &file.Size); err != nil {
+			return nil, err
 		}
 		files = append(files, file)
 	}
-	return
+	return files, nil
 }
 
-func IndexView(w http.ResponseWriter, r *http.Request) {
-	page, err := strconv.Atoi(r.URL.Query().Get("page"))
-	if err != nil || page < 1 {
+func (fs *FileServer) IndexHandler(w http.ResponseWriter, r *http.Request) {
+	page, _ := strconv.Atoi(r.URL.Query().Get("page"))
+	if page < 1 {
 		page = 1
 	}
-	pageSize, err := strconv.Atoi(r.URL.Query().Get("size"))
-	if err != nil || pageSize < 1 {
+	pageSize, _ := strconv.Atoi(r.URL.Query().Get("size"))
+	if pageSize < 1 {
 		pageSize = 100
 	}
 	path := r.URL.Query().Get("path")
-	offset := (page - 1) * pageSize
 	if path == "" {
 		path = "."
 	}
-	files, err := ListFiles(path, offset, pageSize)
+	offset := (page - 1) * pageSize
+
+	files, err := fs.ListFiles(path, offset, pageSize)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -99,12 +101,15 @@ func IndexView(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	initDB()
+	server, err := NewFileServer()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	root := "/Volumes/nsfw"
-	go scanDirectory(root)
+	go server.ScanDirectory(root)
 
-	http.HandleFunc("/list", IndexView)
+	http.HandleFunc("/", server.IndexHandler)
 	log.Println("Server is running on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
