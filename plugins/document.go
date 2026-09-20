dc8c030cf866da50dc1c4b4c43409525fc8919e3
Lsong <hi@lsong.org>
2026-09-20 19:15:12 +0800

    refactor: move media plugin definitions

---
+++ plugins/document.go
@@ -0,0 +1,17 @@
+package plugins
+
+import (
+	"time"
+
+	"github.com/lsongdev/files-go/catalog"
+	"github.com/lsongdev/files-go/model"
+	"github.com/lsongdev/files-go/processor"
+	"github.com/lsongdev/files-go/storage"
+)
+
+func Document(catalog *catalog.Catalog, storages *storage.Registry, thumbnail *processor.Thumbnail, cacheDir, pdfInfo, pdfToPPM string) Plugin {
+	return NewPlugin("document", func(entry model.Entry) bool {
+		return extensionIn(entry, "pdf")
+	}, processor.NewPDFMetadata(catalog, storages, pdfInfo, 30*time.Second),
+		processor.NewPDFThumbnail(catalog, storages, thumbnail, cacheDir, pdfToPPM, 60*time.Second))
+}
