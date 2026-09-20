ba9c37373f5bc08004f97964d338d2377e4fc75e
Lsong <hi@lsong.org>
2026-09-20 19:38:55 +0800

    refactor: wire TMDB artwork inside movies plugin

--- plugins/movies.go
+++ plugins/movies.go
@@ -9,7 +9,13 @@ import (
 // Movies composes semantic movie/TV capabilities. Generic video probing stays
 // in the video plugin; TMDB transport and artwork stay in the tmdb package.
 func Movies(file *processor.MovieFile, directory *processor.MovieDirectory, artwork *tmdb.Artwork) Plugin {
+	steps := []processor.Processor{file}
+	if artwork != nil {
+		directory.SetMatchedHook(artwork.ProcessEntry)
+		steps = append(steps, artwork)
+	}
+	steps = append(steps, directory)
 	return NewPlugin("movies", func(entry model.Entry) bool {
 		return file.Match(entry) || directory.Match(entry)
-	}, file, artwork, directory)
+	}, steps...)
 }
