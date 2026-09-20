3b717c8aa3a930b5728c5e7cd6e6a1e6fcdc8781
Lsong <hi@lsong.org>
2026-09-20 19:15:07 +0800

    refactor: move media plugin definitions

---
+++ plugins/music.go
@@ -0,0 +1,20 @@
+package plugins
+
+import (
+	"time"
+
+	"github.com/lsongdev/files-go/catalog"
+	"github.com/lsongdev/files-go/model"
+	"github.com/lsongdev/files-go/processor"
+	"github.com/lsongdev/files-go/storage"
+)
+
+// Music extracts audio metadata and embedded cover art.
+func Music(catalog *catalog.Catalog, storages *storage.Registry, thumbnail *processor.Thumbnail, ffprobe, ffmpeg string) Plugin {
+	return NewPlugin("music", func(entry model.Entry) bool {
+		return extensionIn(entry, "mp3", "m4a", "aac", "flac", "wav", "ogg", "opus", "wma", "aiff", "ape")
+	},
+		processor.NewFFProbe(catalog, storages, ffprobe, 30*time.Second),
+		processor.NewAudioArtwork(catalog, storages, thumbnail, ffmpeg, 30*time.Second),
+	)
+}
